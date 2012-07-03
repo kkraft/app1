869a3d185dba0ecf83d4d024cc09eea24fdcb8fd
korykraft <kory@kraft>
2012-07-03 14:36:48 -0700

    added users with name and email, and two static pages home, help

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 App1::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
