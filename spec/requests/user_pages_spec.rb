aa6437223a494982b0f5c215702e8deb21a5604a
korykraft <kory@kraft>
2012-07-12 16:58:28 -0700

    added signout, users_path, pagination, faker for sample data

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,11 +4,33 @@ describe "User pages" do
 
   subject { page }
 
+  describe "index" do
+
+    let(:user) {FactoryGirl.create(:user)}
+
+    before(:all) { 30.times { FactoryGirl.create(:user)}}
+    after(:all)  {User.delete_all}
+
+    before do
+     sign_in user
+      visit users_path
+    end
+
+    it {should have_selector('title', text: 'All users')}
+    it {should have_selector('h1', text: 'All users')}
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text => 'Sign up') }
     it { should have_selector('title', text => 'Sign up') }
+
+    it "should list each user" do
+      User.all.each do |user|
+        page.should have_selector('li', text: user.name)
+      end
+    end
   end
 
   describe "profile page" do
@@ -47,8 +69,43 @@ describe "User pages" do
         before {click_button submit}
         it {should have_link 'Sign out'}
       end
+    end
+  end
 
+  describe "edit" do
+    let(:user) {FactoryGirl.create(:user)}
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
 
+    describe "page" do
+      it {should have_selector('h1', text: "Update your profile")}
+      it {should have_selector('title', text: "Edit user")}
+    end
+
+    describe "with invalid info" do
+      before {click_button "Save changes"}
+
+      it { should have_content('error')}
+    end
+
+    describe "with valid info" do
+      let(:new_name) { "New Name"}
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",         with: new_name
+        fill_in "Email",        with: new_email
+        fill_in "Password",     with: user.password
+        fill_in "Confirm Password",     with: user.password
+        click_button "Save changes"
+      end
+
+      it {should have_selector('title', text: new_name)}
+      it {should have_selector('div.alert.alert-success')}
+      it {should have_link('Sign out', href: signout_path)}
+      specify {user.reload.name.should == new_name}
+      specify {user.reload.email.should == new_email}
     end
   end
 end
